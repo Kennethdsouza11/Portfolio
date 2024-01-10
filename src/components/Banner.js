b97ef86921b0ecd603d32b6395b9e00d77582d26
Kenneth <kennethdsouza2004@gmail.com>
2024-01-10 22:46:44 +0530

    made a basic homepage

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import { Col, Container, Row } from "react-bootstrap"
+export const Banner = () => {
+    return (
+        <section className = "banner" id = "home">
+            <Container>
+                <Row className = "align-items-center">
+                    <Col xs = {12} md = {6} xl ={7} >
+                        <span className = "tagline">Welcome to my Portfolio</span>
+                        <h1> { "Hi I'm Kenneth Vian Dsouza " }<span className = "wrap"> Web Developer</span></h1>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+
+    )
+}
+
+export default Banner;
