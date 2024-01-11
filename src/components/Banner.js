bb79015c74fac8e89d81b2fa0b32056164793c3f
Kenneth <kennethdsouza2004@gmail.com>
2024-01-11 12:08:28 +0530

    Added skills and background image animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,59 @@
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import headerImg  from "../assets/img/header-img.svg";
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setisDeleting] = useState(false);
+    const toRotate = ["Novice Web Developer","Machine Learning","Deep Learning","Novice App Developer"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 1000;
+    
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+        
+        if (isDeleting){
+            setDelta(prevDelta => prevDelta/2)
+        }
+
+        if(!isDeleting && updatedText === fullText){
+            setisDeleting(true);
+            setDelta(period);
+        }
+        else if(isDeleting && updatedText === ''){
+            setisDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(100);
+        }
+    }
+
     return (
         <section className = "banner" id = "home">
             <Container>
                 <Row className = "align-items-center">
-                    <Col xs = {12} md = {6} xl ={7} >
+                    <Col xs = {7} md = {6} xl ={7} >
                         <span className = "tagline">Welcome to my Portfolio</span>
-                        <h1> { "Hi I'm Kenneth Vian Dsouza " }<span className = "wrap"> Web Developer</span></h1>
+                        <h1> { "Hi I'm Kenneth Dsouza " }
+                        <p></p><span className="wrap">{text}</span></h1>
+                        <p>jkhbvk jfehbv efkjh vbdjkbv dbvbvjh sbvsjb hvkjhb aksjfhbvk jsahbvfk jhs afbv hybrukh bvjkdsh bvkj dhs bfvjh shjbvsjbvdj dhjfbv dvbd vdbvbhbdj bvbehbe hbvdv jhfbeh egb ehbg ebgjfhgb  hbgjbg g jdhbv dfjvbv eu gfdvbfkg bdskjg fheuyfgh hvbeugb euhbeuyg evbg eu ehbeug ehj bveug bue vbeffvb e</p>
+                        <button onClick = {() => console.log('connect')}>Let's connect! <ArrowRightCircle size = {25}/></button>
+                    </Col>
+                    <Col xs = { 5 } xm = {6} xl = {5}>
+                        <img src = {headerImg} alt = "Header img" />
                     </Col>
                 </Row>
             </Container>
---
+++ src/components/Skills.js
@@ -0,0 +1,62 @@
+import { Container, Row, Col} from "react-bootstrap"
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import colorSharp from "../assets/img/color-sharp.png";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+export const Skills = () => {
+    const percentage_WD = 40;
+    const percentage_ML = 80;
+    const percentage_DL = 80;
+    const percentage_AD = 40;
+    const responsive = {
+        superLargeDesktop: {
+          // the naming can be any, depends on you.
+          breakpoint: { max: 4000, min: 3000 },
+          items: 5
+        },
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+      };
+
+      return (
+        <section className = "skill" id = "skills">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className = "skill-bx">
+                        <h2>
+                            Skills
+                        </h2>
+                        <Carousel responsive={responsive} infinite = {true} className = "skills-slider">
+                            <div className = "item">
+                            <CircularProgressbar value={percentage_WD} text={`${percentage_WD}%`} /> <h5>Web Developer</h5>
+                            </div>
+                            <div className = "item">
+                            <CircularProgressbar value={percentage_ML} text={`${percentage_ML}%`} /> <h5>Machine Learning</h5>
+                            </div>
+                            <div className = "item">
+                            <CircularProgressbar value={percentage_DL} text={`${percentage_DL}%`} /> <h5>Deep Learning</h5>
+                            </div>
+                            <div className = "item">
+                            <CircularProgressbar value={percentage_AD} text={`${percentage_AD}%`} /> <h5>App Developer</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className = "background-image-left" src={colorSharp} />
+        </section>
+      )
+}
