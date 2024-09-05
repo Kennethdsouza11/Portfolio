7a4830442f02654ad87cc6107b096ceeba198a8e
Kenneth Dsouza <kennethdsouza2004@gmail.com>
2024-09-05 14:12:01 +0530

    updated the path for an image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/kenneth_pic.jpg";
+import profilePic from "../assets/kenneth_pic.JPG";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
